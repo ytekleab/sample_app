4c0b84deb1187c227d07831efc6ae2c07b22f654
Yonatan Tekleab <ytekleab@gmail.com>
2013-10-11 02:02:50 -0500

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name      "Yonatan Tekleab"
+    email     "ytekleab@example.com"
+    password  "foobar"
+    password_confirmation "foobar"
+  end
+end
